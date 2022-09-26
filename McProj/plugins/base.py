76be05028d7fea706de2b2124fd0166adaaf0aac
treel <treel@DESKTOP-I3O7IAM>
2022-09-25 19:21:32 -0600

    Added some names for keys.

--- McProj/plugins/base.py
+++ McProj/plugins/base.py
@@ -1,2 +1,7 @@
+import minecraft_editor as mc
 def mouseMoved(vector):
     print(vector.x, vector.y)
+    e = mc.vec3(0.0, 4.0, 3.0)
+def keyPressed(key):
+    if key == mc.KEY_SPACE:
+        print("SPACE IS WACK!")
